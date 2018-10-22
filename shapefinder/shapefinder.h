daa91cddf71c0ad3ab0d030ef081f7ae375e46a0
DPain <d.pain.coreymu@gmail.com>
2018-10-22 14:26:40 -0400

    Initial Commit

---
+++ shapefinder/shapefinder.h
@@ -0,0 +1,10 @@
+﻿// shapefinder.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+void load_arguements(int argc, char* argv[]);
+void detect_shape();
