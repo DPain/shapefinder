b4fd9be193768bc3f07827c63246f89b1c2e8110
DPain <d.pain.coreymu@gmail.com>
2018-10-25 01:37:13 -0400

    Added Method to detect Rectangle, Circle. TODO: Differentiate Squares from Rectangles.

--- shapefinder/shapefinder.h
+++ shapefinder/shapefinder.h
@@ -3,7 +3,18 @@
 
 #pragma once
 
+#include <iostream>
+#include <map>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
 
 // TODO: Reference additional headers your program requires here.
 void load_arguments(int argc, char* argv[]);
 void detect_shape();
+short* get_rgb(string hex_string);
